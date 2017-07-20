c4e422ca3c1e9306f03f767db8b0ac110e516e64
Jack Pope <jackpope1@gmail.com>
2017-07-19 21:36:30 -0400

    small test cleanup

--- test/mocha/features/helpers.js
+++ test/mocha/features/helpers.js
@@ -1,8 +1,8 @@
-const expect = chai.expect;
-const videoHTML = '<video id="main_player" class="video-js" controls preload="auto" width="720" height="405" data-setup="{}"><source src="./../../contently-platform-overview.mp4" type="video/mp4"></video>'
-const addVideo = () => $('#video-container').html(videoHTML);
-const initVJS = () => videojs('main_player');
-const firstPlay = () => $('.vjs-big-play-button').click();
+const expect               = chai.expect;
+const videoHTML            = '<video id="main_player" class="video-js" controls preload="auto" width="720" height="405" data-setup="{}"><source src="./../../contently-platform-overview.mp4" type="video/mp4"></video>'
+const addVideo             = () => $('#mocha-video-container').html(videoHTML);
+const initVJS              = () => videojs('main_player');
+const firstPlay            = () => $('.vjs-big-play-button').click();
 const toggleAnnotationMode = () => $('.vac-player-btn').click();
 
 var player, plugin;
