9171d0cf1c46981ee679d16b7d33acb7208924ef
Jack Pope <jackpope1@gmail.com>
2020-04-07 13:09:53 -0400

    fix(demo): Replace missing demo video with test video

--- docs/_site/assets/js/demo_annotations.js
+++ docs/_site/assets/js/demo_annotations.js
@@ -1,66 +1,65 @@
-(function() {
-    demoAnnotations = [
+(function () {
+  window.demoAnnotations = [
+    {
+      id: 1,
+      range: {
+        start: 12,
+        end: 16
+      },
+      shape: null,
+      comments: [
         {
-            id: 1,
-            range: {
-                start: 10,
-                end: 16
-            },
-            shape: null,
-            comments: [
-                {
-                    id: 1,
-                    meta: {
-                        datetime: "2017-03-28T19:17:32.238Z",
-                        user_id: 1,
-                        user_name: "Jack"
-                    },
-                    body: "I like having the pictures here breaking up the video but it feels a bit short lived. Perhaps add one or two more?"
-                },
-                {
-                    id: 4,
-                    meta: {
-                        datetime: "2017-03-28T19:17:32.238Z",
-                        user_id: 2,
-                        user_name: "Blaise"
-                    },
-                    body: "I agree but this section is too fast for more pictures. I can try moving them later in the video."
-                }
-            ]
+          id: 1,
+          meta: {
+            datetime: '2017-03-28T19:17:32.238Z',
+            user_id: 1,
+            user_name: 'Jack'
+          },
+          body: 'Can we swap these for the new icons?'
         },
         {
-            id: 2,
-            range: {
-                start: 1,
-                stop: 1
-            },
-            shape: {
-                x1: 34,
-                y1: 28,
-                x2: 65,
-                y2: 71
-            },
-            comments: [
-                {
-                    id: 2,
-                    meta: {
-                        datetime: "2017-03-28T19:17:32.238Z",
-                        user_id: 1,
-                        user_name: "Jack"
-                    },
-                    body: "Can you make this logo bigger?"
-                },
-                {
-                    id: 3,
-                    meta: {
-                        datetime: "2017-03-28T19:17:32.238Z",
-                        user_id: 2,
-                        user_name: "Blaise"
-                    },
-                    body: "Sure, I'll make it fill the screen some more in the next draft."
-                }
-
-            ]
+          id: 4,
+          meta: {
+            datetime: '2017-03-28T19:17:32.238Z',
+            user_id: 2,
+            user_name: 'Blaise'
+          },
+          body: 'Yes I will update this afternoon.'
         }
-    ];
+      ]
+    },
+    {
+      id: 2,
+      range: {
+        start: 1,
+        stop: 1
+      },
+      shape: {
+        x1: 34,
+        y1: 28,
+        x2: 65,
+        y2: 71
+      },
+      comments: [
+        {
+          id: 2,
+          meta: {
+            datetime: '2017-03-28T19:17:32.238Z',
+            user_id: 1,
+            user_name: 'Jack'
+          },
+          body: 'Can you make this logo bigger?'
+        },
+        {
+          id: 3,
+          meta: {
+            datetime: '2017-03-28T19:17:32.238Z',
+            user_id: 2,
+            user_name: 'Blaise'
+          },
+          body: "Sure, I'll make it fill the screen some more in the next draft."
+        }
+      ]
+    }
+  ];
 })();
--- docs/_site/assets/js/main.js
+++ docs/_site/assets/js/main.js
@@ -1,46 +1,50 @@
 // Setup player and plugin
-(function(){
-    var player = videojs('the_video', {
-        controlBar: {
-            volumePanel: { inline: false }
-        }
-    });
-    player.muted(true);
-    player.annotationComments({
-        annotationsObjects: demoAnnotations,
-        bindArrowKeys: true,
-        meta: {
-            user_id: 3,
-            user_name: "Guest"
-        }
-    });
+(function () {
+  const player = window.videojs('the_video', {
+    controlBar: {
+      volumePanel: { inline: false }
+    }
+  });
+  player.muted(true);
+  player.annotationComments({
+    annotationsObjects: window.demoAnnotations,
+    bindArrowKeys: true,
+    meta: {
+      user_id: 3,
+      user_name: 'Guest'
+    }
+  });
 })();
 
 // Intercept VAC logs and port them to console UI
-(function(){
-    window.VAC_DEBUG = true;
-    var $console = $(".console"),
-        consoleLog = console.log;
-    console.log = function (msg) {
-        if (msg === "::VAC::") {
-            var output = "";
-            for(var i = 0; i <= arguments.length; i++) {
-                output = output + " " + JSON.stringify(arguments[i]);
-            };
+(function () {
+  window.VAC_DEBUG = true;
+  const $console = $('.console');
+  // eslint-disable-next-line no-console
+  const consoleLog = console.log;
 
-            // Remove extra quotes and any undefined
-            output = output.replace(/\"/g, "").trim();
-            output = output.replace("undefined", "");
-            output = ">> " + output;
+  // eslint-disable-next-line no-console
+  console.log = function (msg) {
+    if (msg === '::VAC::') {
+      let output = '';
 
-            var $p = $("<p/>").text(output);
-            $console.append($p);
-            $console.scrollTop($console[0].scrollHeight)
-        }
-        consoleLog.apply(console, arguments);
-    };
+      for (let i = 0; i <= arguments.length; i++) {
+        output = `${output} ${JSON.stringify(arguments[i])}`;
+      }
+
+      // Remove extra quotes and any undefined
+      output = output.replace(/\"/g, '').trim();
+      output = output.replace('undefined', '');
+      output = `>> ${output}`;
+
+      const $p = $('<p/>').text(output);
+      $console.append($p);
+      $console.scrollTop($console[0].scrollHeight);
+    }
+    consoleLog.apply(console, arguments);
+  };
 })();
 
-$(".clear-console-btn").on("click", function() {
-    $(".console").empty();
+$('.clear-console-btn').on('click', function () {
+  $('.console').empty();
 });
